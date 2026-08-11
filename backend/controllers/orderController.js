e674efe24c9732d6600484e7b84b0bb0563506ff
Srideep <srideeplucky64@gmail.com>
2026-08-11 11:44:21 +0530

    Change in few calculations(cart page)

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -40,7 +40,7 @@ const placeOrder = async (req, res) => {
                 product_data:{
                     name:"Delivery Charges"
                 },
-                unit_amount:50*100
+                unit_amount:80*100
             },
             quantity:1
         })
